00939cbc6fb9092d7bcfcbb12ab71cbc1a99ab16
Sabrina Prichard-Lybeck <sp223kz@student.lnu.se>
2024-09-04 11:22:59 +0200

    Start of method to fetch current time from world time api

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,4 @@
 // Will present a form for the user to input their name. 
 // Once form has been sent the user will be greeted with a customised welcome message and the current date and time will be displayed and the background image will differ at daytime and at nighttime.
+
+import { fetchTime } from '../modules/timeZoneFetcher.js'
--- src/modules/timeZoneFetcher.js
+++ src/modules/timeZoneFetcher.js
@@ -1,1 +1,28 @@
-// Fetches info about current time via ipgeolocation time zone API.
+/**
+ *
+ *
+ * @class TimeZoneFetcher
+ */
+export class TimeZoneFetcher {
+  /**
+   *
+   * @function - fetches info about current time via world time open api.
+   */
+  async fetchTime () {
+    try {
+      const url = 'http://worldtimeapi.org/api/timezone/Europe/Stockholm'
+
+      const response = await fetch(url)
+
+      if (!response.ok) {
+        throw new Error(response.status)
+      }
+
+      const timeData = response.json()
+
+      console.log(timeData)
+    } catch (error) {
+      // Error handling.
+    }
+  }
+}
