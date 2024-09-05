faf0cfd76a2831bccf996dc73e3e78692e0fe08f
Sabrina Prichard-Lybeck <sp223kz@student.lnu.se>
2024-09-05 17:20:29 +0200

    Removed inline comments

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,3 @@
-// Will present a form for the user to input their name. 
-// Once form has been sent the user will be greeted with a customised welcome message and the current date and time will be displayed and the background image will differ at daytime and at nighttime.
-
 import { TimeFetcher } from '../modules/TimeFetcher.js'
 
 document.addEventListener('DOMContentLoaded', () => {
